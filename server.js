5668034610633a96ddfdb634db5ee9136eb69350
Glitch (giant-yew) <none>
2018-02-15 10:08:55 +0000

    🌲🎊 Checkpoint server.js:667310/65

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var express = require('express');
 var app = express();
 
 var GoogleSearch = require('google-search');
+var GoogleImages = require('google-images');
 
 var CX = '007483715269021992219:tsmgvdhde94';
 var API_KEY = 'AIzaSyBG5wSjljgM7qNPmsTLtKptf36Cz2WvtwU';
@@ -27,18 +28,24 @@ app.get("/api/imagesearch/:query", function (request, response) {
   var keyword = request.params["query"];
   var offset = request.query.offset;
   
+  var client = new GoogleImages(CX, API_KEY);
+  
+  client.search( keyword, function (err, images) {
+    console.log(images)
+});
+  
   var googleSearch = new GoogleSearch({
   key: API_KEY,
   cx: CX
 });
   
-  googleSearch.build({
-  q: keyword,
-  start: 1,
-  num: 10
-}, function(error, result) {
-  response.send(result);
-});
+//  googleSearch.build({
+//  q: keyword,
+//  start: 1,
+//  num: 10
+//}, function(error, result) {
+//  response.send(result);
+//});
 
   
 });
