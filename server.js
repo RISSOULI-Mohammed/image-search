f650f5d4c8a12465e334123f61f3c22a1df402b8
Glitch (giant-yew) <none>
2018-02-16 08:46:27 +0000

    🍹✌️ Checkpoint server.js:667310/316

--- server.js
+++ server.js
@@ -5,7 +5,10 @@
 var express = require('express');
 var app = express();
 
-var request = require('request');
+var req = require('request');
+
+var API_KEY = 'AIzaSyBG5wSjljgM7qNPmsTLtKptf36Cz2WvtwU';
+var CX = '007483715269021992219:tsmgvdhde94';
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
@@ -22,10 +25,20 @@ app.get("/api/imagesearch/:query", function (request, response) {
   var keyword = request.params["query"];
   var offset = request.query.offset;
   
+    var apiUrl = 'https://www.googleapis.com/customsearch/v1?key=' + API_KEY + '&cx=' + CX + '&q=' + keyword + '&searchType=image' + '&fields=items(link,snippet,image/thumbnailLink,image/contextLink)';
+  req(apiUrl, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var outPutJson = JSON.parse(body);
+      response.send(body);
+    }
+  });
+
+  
 });
 
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
+});port);
 });
